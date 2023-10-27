991d13256a61eb622c6af84f4b6fcd1c3dde6fd1
Will Hoppin <whoppin@middlebury.edu>
2023-10-27 16:33:07 -0400

    remove

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d5b448a67595bd6"; }
+/******/ 		__webpack_require__.h = function() { return "beeaaa901dde3ed6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
