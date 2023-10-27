6cf7fc2855ec0cfa8469511bee34b2a956d7a8d1
Will Hoppin <whoppin@middlebury.edu>
2023-10-27 16:33:42 -0400

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "beeaaa901dde3ed6"; }
+/******/ 		__webpack_require__.h = function() { return "4114873f012e7818"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
